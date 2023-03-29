889d29ab7ea8ab451266f234895f9b648625463f
EduardoTn <49573663+EduardoTn@users.noreply.github.com>
2023-03-29 15:14:57 -0300

    hurt player

--- Assets/Script/ControllerGame.cs
+++ Assets/Script/ControllerGame.cs
@@ -10,11 +10,20 @@ public class ControllerGame : MonoBehaviour
     public Text txtScore;
     public AudioSource fxGame;
     public AudioClip fxCenoura;
+    public GameObject explosion;
+    public AudioClip fxDestroyEnemie;
+    public Sprite[] lifes;
+    public Image actualLife;
     public void Pontuacao(int qtdPontos)
     {
         Score += qtdPontos;
         txtScore.text = Score.ToString();
         fxGame.PlayOneShot(fxCenoura);
+    }
+
+    public void BarraVida(int life)
+    {
+        actualLife.sprite = lifes[life];
     }
     void Start()
     {
--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -6,6 +6,8 @@ public class PlayerMove : MonoBehaviour
 {
     private Animator playerAnim;
     private Rigidbody2D playerRig;
+    private SpriteRenderer srPlayer;
+    private bool invencibility = false;
     public Transform groundCheck;
     public bool isGrounded = false;
     public float Speed;
@@ -15,12 +17,14 @@ public class PlayerMove : MonoBehaviour
     private ControllerGame _control;
     public AudioSource fxGame;
     public AudioClip fxJump;
-    public AudioClip fxDestroyEnemie;
+    public int vidas = 3;
+    public Color hitColor;
     void Start()
     {
 
         playerAnim = GetComponent<Animator>();
         playerRig = GetComponent<Rigidbody2D>();
+        srPlayer = GetComponent<SpriteRenderer>();
         _control = FindObjectOfType<ControllerGame>();
     }
     void Update()
@@ -71,6 +75,40 @@ public class PlayerMove : MonoBehaviour
         playerAnim.SetFloat(Animation, Validation);
     }
 
+    void Hurt()
+    {
+        if (!invencibility)
+        {
+            invencibility = true;
+            vidas--;
+            StartCoroutine("Dano");
+            _control.BarraVida(vidas);
+        }
+    }
+
+    IEnumerator Dano()
+    {
+        srPlayer.color = hitColor;
+        yield return new WaitForSeconds(0.1f);
+        for (float i = 0; i<1; i += 0.1f)
+        {
+            srPlayer.enabled= false;
+            yield return new WaitForSeconds(0.1f);
+            srPlayer.enabled = true;
+            yield return new WaitForSeconds(0.1f);
+        }
+        srPlayer.color= Color.white;
+        invencibility = false;
+    }
+
+    void enemieHurt()
+    {
+        GameObject tempExplosao = Instantiate(_control.explosion, new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z), transform.localRotation);
+        Destroy(tempExplosao, 0.5f);
+        fxGame.PlayOneShot(_control.fxDestroyEnemie);
+        playerRig.AddForce(new Vector2(0f, JumpForce * 1.5f), ForceMode2D.Impulse);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         switch (collision.gameObject.tag)
@@ -80,11 +118,22 @@ public class PlayerMove : MonoBehaviour
                 Destroy(collision.gameObject);
                 break;
             case "Enemie":
+                enemieHurt();
                 Destroy(collision.gameObject);
-                playerRig.AddForce(new Vector2(0f, JumpForce*1.8f), ForceMode2D.Impulse);
-                fxGame.PlayOneShot(fxDestroyEnemie);
                 break;
             default: 
+                break;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Enemie":
+                Hurt();
+                break;
+            default:
                 break;
         }
     }
